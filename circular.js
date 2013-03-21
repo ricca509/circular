e20b199048da21c5e1f7ee44f48682f5f5468863
Riccardo Coppola <coppola.riccardo@gmail.com>
2013-03-21 22:19:16 +0100

    Code reformatted

--- circular.js
+++ circular.js
@@ -1,49 +1,49 @@
-var Circular = (function() {
-	"use strict";
+var Circular = (function () {
+    "use strict";
 
-	var module = function(array) {
-		this._array = array || [];
-		this._index = 0;
-	};
+    var module = function (array) {
+        this._array = array || [];
+        this._index = 0;
+    };
 
-	module.prototype.constructor = module;    
+    module.prototype.constructor = module;
 
-	module.prototype.add = function(item) {
-		return this._array.push(item);
-	};
+    module.prototype.add = function (item) {
+        return this._array.push(item);
+    };
 
-	module.prototype.next = function() {		
-		if (this._index < this._array.length) {
-			this._index = (this._index + 1) % this._array.length;
-			return this._array[this._index];		
-		}
+    module.prototype.next = function () {
+        if (this._index < this._array.length) {
+            this._index = (this._index + 1) % this._array.length;
+            return this._array[this._index];
+        }
 
-		return undefined;
-	};
+        return undefined;
+    };
 
-	module.prototype.prev = function() {
+    module.prototype.prev = function () {
         var ret = this._array[this._array.length - this._index - 1];
         this._index = (this._index + 1) % this._array.length;
         return ret;
 
-		return undefined;
-	};
+        return undefined;
+    };
 
-	module.prototype.getArray = function() {
-		return this._array;
-	};
+    module.prototype.getArray = function () {
+        return this._array;
+    };
 
-	module.prototype.toString = function() {
-		return "The circular array has " + this._array.length + " items: " + this._array.toString(); 
-	};
+    module.prototype.toString = function () {
+        return "The circular array has " + this._array.length + " items: " + this._array.toString();
+    };
 
-	return module;
+    return module;
 
 })();
 
 var buffer = new Circular(['a', 'b', 'c', 'd', 'e', 'f']);
 
-for(var i = 0; i < 10; i++) {
+for (var i = 0; i < 10; i++) {
     console.log(buffer.prev() + ' - ' + buffer._index);
 }
 
